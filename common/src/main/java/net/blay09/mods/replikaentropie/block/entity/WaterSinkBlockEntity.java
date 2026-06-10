4969319fea5142d4be959395595b0acfa0a33b14
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-10 09:46:08 +0200

    feat: Allow decontamination of fragmental waste carts

--- common/src/main/java/net/blay09/mods/replikaentropie/block/entity/WaterSinkBlockEntity.java
+++ common/src/main/java/net/blay09/mods/replikaentropie/block/entity/WaterSinkBlockEntity.java
@@ -2,12 +2,14 @@ package net.blay09.mods.replikaentropie.block.entity;
 
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.platform.capabilities.CommonCapabilities;
+import net.blay09.mods.replikaentropie.entity.FragmentalWasteMinecart;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.AABB;
 
 public class WaterSinkBlockEntity extends BlockEntity {
     public WaterSinkBlockEntity(BlockPos pos, BlockState state) {
@@ -29,6 +31,15 @@ public class WaterSinkBlockEntity extends BlockEntity {
             }
             if (blockEntity instanceof FragmentalGeneratorBlockEntity fragmentalGeneratorBlockEntity) {
                 fragmentalGeneratorBlockEntity.adjustTemperature(-0.1f);
+                return;
+            }
+
+            final var minecarts = level.getEntitiesOfClass(FragmentalWasteMinecart.class, new AABB(currentPos));
+            if (!minecarts.isEmpty()) {
+                final var minecart = minecarts.getFirst();
+                if (minecart.decontaminationTick()) {
+                    minecart.convertToWasteBarrel();
+                }
                 return;
             }
 
--- common/src/main/java/net/blay09/mods/replikaentropie/entity/FragmentalWasteMinecart.java
+++ common/src/main/java/net/blay09/mods/replikaentropie/entity/FragmentalWasteMinecart.java
@@ -1,20 +1,57 @@
 package net.blay09.mods.replikaentropie.entity;
 
 import net.blay09.mods.replikaentropie.block.ModBlocks;
+import net.blay09.mods.replikaentropie.block.entity.FragmentalWasteBlockEntity;
 import net.blay09.mods.replikaentropie.core.waste.FragmentalWaste;
 import net.blay09.mods.replikaentropie.item.ModItems;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.minecart.AbstractMinecart;
-import net.minecraft.world.entity.vehicle.minecart.Minecart;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 
 public class FragmentalWasteMinecart extends AbstractMinecart {
+    private int decontaminationTicks;
 
     public FragmentalWasteMinecart(EntityType<? extends FragmentalWasteMinecart> entityType, Level level) {
         super(entityType, level);
+    }
+
+    public boolean decontaminationTick() {
+        if (decontaminationTicks < FragmentalWasteBlockEntity.DECONTAMINATION_TICKS_REQUIRED) {
+            decontaminationTicks++;
+            if (level() instanceof ServerLevel serverLevel) {
+                if (decontaminationTicks % 20 == 0) {
+                    serverLevel.sendParticles(ParticleTypes.DUST_PLUME, getX(), getY() + 0.5, getZ(), 4, 0.75f, 0.75f, 0.75f, 0);
+                }
+                if (decontaminationTicks % 40 == 0) {
+                    serverLevel.playSound(null, blockPosition(), SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS, 1f, (float) (0.5 + Math.random()));
+                }
+            }
+        }
+        return decontaminationTicks >= FragmentalWasteBlockEntity.DECONTAMINATION_TICKS_REQUIRED;
+    }
+
+    public void convertToWasteBarrel() {
+        if (level() instanceof ServerLevel serverLevel) {
+            serverLevel.sendParticles(ParticleTypes.SOUL, getX(), getY() + 1.25, getZ(), 4, 0.25f, 0.25f, 0.25f, 0);
+            serverLevel.playSound(null, blockPosition(), SoundEvents.ALLAY_DEATH, SoundSource.BLOCKS, 1f, (float) (0.5 + Math.random()));
+
+            final var wasteBarrelMinecart = new WasteBarrelMinecart(ModEntities.wasteBarrelMinecart.value(), serverLevel);
+            wasteBarrelMinecart.setPos(getX(), getY(), getZ());
+            wasteBarrelMinecart.setYRot(getYRot());
+            wasteBarrelMinecart.setXRot(getXRot());
+            wasteBarrelMinecart.setDeltaMovement(getDeltaMovement());
+            serverLevel.addFreshEntity(wasteBarrelMinecart);
+            discard();
+        }
     }
 
     @Override
@@ -43,5 +80,17 @@ public class FragmentalWasteMinecart extends AbstractMinecart {
     @Override
     public ItemStack getPickResult() {
         return new ItemStack(ModItems.fragmentalWasteMinecart.asItem());
+    }
+
+    @Override
+    protected void addAdditionalSaveData(ValueOutput output) {
+        super.addAdditionalSaveData(output);
+        output.putInt("DecontaminationTicks", decontaminationTicks);
+    }
+
+    @Override
+    protected void readAdditionalSaveData(ValueInput input) {
+        super.readAdditionalSaveData(input);
+        decontaminationTicks = input.getIntOr("DecontaminationTicks", 0);
     }
 }
